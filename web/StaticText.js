8cb915663e9afc4044a2288b4262572b63a619ef
rmgibbo <rmgibbo@wetfe.com>
2020-01-25 18:53:17 -0700

    build process for static pages

---
+++ web/StaticText.js
@@ -0,0 +1,53 @@
+/*
+ *  STATIC TEXT (ST)
+ *
+ *  "i" prefix indicates INFO
+ */
+const TEXT = {
+
+    iPROPER_AGE: 'Number of gigaseconds (billions of seconds) passed since your birth, measured in proper time.',
+
+    iLIVING_AGE: `Amount of proper_age that you have consciously experienced. 
+  For beings with humanoid brains, this generally accounts for the percent of your life spent sleeping and in cryostasis.`,
+
+    iCOMMON_CALENDAR_BIRTHDAY: 'The first number is the estimated (using a standardized method) \
+  number of petaseconds (quadrillions of seconds) that have elapsed since the Confluence, measured in proper time. \
+  The second number is a logarithmic scalar that attempts to account for the difference between \
+  local (proper) clocks and those of the Common Reference Frame, as well as the problem of simultaneity. \
+  E.g., the farther away you were (at your birth) deemed to be from the CRF, the larger this scalar will be. \
+  Large ~scalar values therefore represent a relative ambiguity in the first number; \
+  and a ~scalar of 0.0 indicates beings born precisely within the CRF.',
+
+    iCOMMON_CALENDAR_AGE: 'Calculated using a standardized method, this number is meant to \
+  represent the number of seconds a being living constantly in the CRF would have \
+  experienced (in CRF time) during <em>your</em> life (as accounted by your proper age). \
+  There is necessarily a -- potentially extreme -- amount of ambiguity in such a calculation, \
+  and thus this age is used as a rough guide to understanding alien beings.',
+
+    iPROPER_HEIGHT: 'TODO',
+
+    iCOMMON_HEIGHT: 'TODO',
+
+    iEXPERIENCE: 'TODO. Loosely coupled experience system. experience accumulates in small integer amounts, \
+  typically 1 per encounter. Gaining two or more at once is rare, reserved for truly remarkable and impressionable \
+  events. Accumulated experience may be used as a guide (as tightly coupled as the GM wishes) for the \
+  level/ascention system, but may be decoupled entirely.',
+
+    iSOUL_POOL: 'TODO',
+
+    iPROPER_MASS: 'Number of kilograms (thousands of grams) of matter taken to comprise <em>you</em>, \
+  in a reference-frame-invariant way.',
+
+    iGROWTH: 'TODO',
+
+    iCORE_ATTRIBUTES: 'TODO',
+
+    iCON: 'Measure of your physical health, strength, endurance, and tolerance to bodily poisons.',
+
+    iDEX: 'Measure of your physical agility, reflexes, acrobatics, and finesse with weapons & tools.',
+
+    iINT: 'Measure of your mental agility, perception, insight, logic, and detailed memory.',
+
+    iWIL: 'Measure of your mental health, mettle, and strength of character & convictions.'
+
+};
